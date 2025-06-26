c53733884a15ff9409ede94bf71070db798a03be
yonesYN <132742251+yonesYN@users.noreply.github.com>
2025-06-26 05:32:56 -0800

    1.8

--- Tools_Optimizer.bat
+++ Tools_Optimizer.bat
@@ -3,7 +3,7 @@ set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 GOTO Main
 :Main
-	TITLE %~n0 1.7.2
+	TITLE %~n0 1.8
 	COLOR B
 	mode con: cols=83 lines=17
 	CLS
